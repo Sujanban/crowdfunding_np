891d148cbc90a07273df57e1d817bb253ba9d39a
Sujan Ban <bansujan@gmail.com>
2024-06-10 20:23:15 +0545

    code optimized

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,11 +7,9 @@ import { Provider } from 'react-redux'
 import store from './app/store/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>
-  // </React.StrictMode>,
 )
